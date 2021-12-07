afc29025149a80ac6c3684f59eb214d38eb5005a
chaudharikomal0003 <chaudharikomal0003@gmail.com>
2021-12-07 12:40:53 +0530

    First commit

---
+++ src/main/java/newtoall/new_to_sample/App.java
@@ -0,0 +1,13 @@
+package newtoall.new_to_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
